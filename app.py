8c4625d1ba7ca76d6a99f57482ba3648c64a87aa
Your Name <your-email@example.com>
2026-03-30 15:09:26 -0400

    Final Test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>RHEL 9.6 CI/CD Pipeline is Live!</h1><p>Status: Successfully Deployed via Jenkins & Podman!!!! Version 2.0</p>"
+    return "<h1>RHEL 9.6 CI/CD Pipeline is Live!</h1><p>Status: Successfully Deployed via Jenkins & Podman! "V1.0 - Fully Automated by Muhammad Sabir"</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
